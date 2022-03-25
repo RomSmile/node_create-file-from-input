234929d7dd41d435533e747ed65affda4d60b3df
Roman Yacenko <ryacenko3@gmail.com>
2022-03-25 20:26:46 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.Interface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function createUserFile() {
+  terminal.question('Please enter the file name ', (nameOfFile) => {
+    terminal.question('Enter the content to the file ', (content) => {
+      fs.writeFile(`./src/files/${nameOfFile}.txt`, content, (err) => {
+        if (err) {
+          // eslint-disable-next-line no-console
+          console.log(err);
+        }
+      });
+
+      terminal.close();
+    });
+  });
+}
+
+createUserFile();
